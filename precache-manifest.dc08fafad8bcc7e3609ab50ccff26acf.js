7b07ce7260f1352cd32a0f12374d887d26c128db
Marah <Marah_rayan@yahoo.com>
2020-06-21 14:03:38 +0100

    Updates

---
+++ precache-manifest.dc08fafad8bcc7e3609ab50ccff26acf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65c3c25bbeacda1e8a8f616e9a7e7876",
+    "url": "/React-Milestone2/index.html"
+  },
+  {
+    "revision": "3756d6b013c25fd1ea55",
+    "url": "/React-Milestone2/static/css/main.968cd1e0.chunk.css"
+  },
+  {
+    "revision": "71cb4ada6bf1665219a9",
+    "url": "/React-Milestone2/static/js/2.3043dd03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Milestone2/static/js/2.3043dd03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3756d6b013c25fd1ea55",
+    "url": "/React-Milestone2/static/js/main.8985a19f.chunk.js"
+  },
+  {
+    "revision": "c7b0cb1b7a90cb6761ab",
+    "url": "/React-Milestone2/static/js/runtime-main.f7fd8474.js"
+  }
+]);
